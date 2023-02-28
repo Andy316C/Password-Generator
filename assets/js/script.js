4876c4ef5cc5bdc0ee1e7b5bea7c39708b6dadd8
andrew clark <andy-clark316@hotmail.com>
2023-02-28 15:03:54 +1100

    added index.html and script.js files

---
+++ assets/js/script.js
@@ -0,0 +1,130 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+
+function generatePassword(){
+  console.log("hello");
+  
+  var com = prompt("Please Enter:\n L for an A-Z password OR \n N for a Numbered password OR B for Both");
+  
+  if(com === "L"){
+    var lengths = prompt("How many letters would you like to have");
+    var specialChar =  prompt("Would you like to include special characters:\n Y = yes N= no");
+    var upperLower = prompt("Please Enter: /nU for upper case OR /nL for lower case OR B for both");
+    var alphabet= ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+    var alphabet1= ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','!','@','#','$','&','*','%','?'];
+      console.log(lengths)
+        var alpw="";
+        var pw=0;
+        for( var i=0;i < lengths; i++){
+          if(specialChar === "Y"){
+              pw= Math.floor(Math.random() * alphabet1.length);
+              // alpw += alphabet1[pw]; 
+              if(upperLower === "U"){
+                  alpw += alphabet1[pw].toUpperCase();
+              }else if(upperLower == "B"){
+                  if(i % 2){
+                  alpw += alphabet1[pw].toUpperCase();
+                  }else{
+                  alpw += alphabet1[pw];
+                  }
+              }else{
+                alpw += alphabet1[pw];
+              }
+
+          }else{
+            if(upperLower === "U"){
+              pw= Math.floor(Math.random() * alphabet.length);
+              alpw += alphabet[pw].toUpperCase();
+            }else if(upperLower === "B"){
+              if(i % 2){
+                pw= Math.floor(Math.random() * alphabet.length);
+                alpw += alphabet[pw].toUpperCase();
+              }else{
+                pw= Math.floor(Math.random() * alphabet.length);
+                alpw += alphabet[pw];
+              }
+            }else{
+              pw= Math.floor(Math.random() * alphabet.length);
+              alpw += alphabet[pw];
+            }
+          }
+        }
+        return alpw;
+  }else if (com === "N"){
+  
+      var lengths = prompt("How many Numbers would you like to have");
+      while(!parseInt(lengths)){
+          console.log("failed");
+          lengths = prompt("Please enter a Number");
+      }
+      var specialChar =  prompt("Would you like to include special characters with your numbers");
+      var alphabet1= ['!','@','#','$','&','*','%','?','_',"+"];
+        console.log(lengths)
+        
+          var alpw="";
+          for( var i=0;i < lengths; i++){
+            var pw= Math.floor(Math.random() * 10);
+            if(specialChar === "Y"){
+              if(i % 2){
+                var stringNum= pw.toString();
+  
+              }else{
+                var stringNum=  alphabet1[pw];
+              }
+            }else{
+              var stringNum= pw.toString();
+            }
+            
+            alpw += stringNum;
+          }
+        
+          console.log(alpw);
+          return alpw;
+
+  }else if(com === "B"){
+  
+    var lengths = prompt("How many characters would you like to have");
+    var specialChar =  prompt("Would you like to include special characters: Y= yes N= no");
+    var alphabet= ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+    var alphabet1= ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','!','@','#','$','&','*','%','?'];
+    
+      console.log(lengths)
+        var alpw="";
+        for( var i=0;i < lengths; i++){
+          
+          var pw  = Math.floor(Math.random() * 10);
+          var pw1 = Math.floor(Math.random() * alphabet.length);
+          var pw2 = Math.floor(Math.random() * alphabet1.length);
+          // console.log(alphabet[pw1]);
+          if(specialChar === "Y"){
+            if(i % 2){
+              var stringNum= alphabet1[pw2];
+            }else{
+              var stringNum= pw;
+            }
+          }else{
+            if(i % 2){
+              var stringNum= alphabet[pw1];
+            }else{
+              var stringNum= pw;
+            }
+          }
+          console.log(stringNum);
+          alpw += stringNum;
+        }
+        return alpw;
+  }
+}
+
+// Add event listener to generate button
+
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
+}
+
+generateBtn.addEventListener("click", writePassword);
+
